de5419eba1876c9e5176e61ade49ec51b48a6fef
Jeff George <webdevjeffus@gmail.com>
2016-02-28 22:52:55 -0500

    Commit incomplete solution-in-progress for no_repeats.js

--- no_repeats.js
+++ no_repeats.js
@@ -1,3 +1,16 @@
+// Solution in progress; not complete
+
+function permute(str) {
+  var chars = str.split("");
+  var perms = [];
+
+  for (var i = 0; i < chars.length; i++ ) {
+
+  }
+
+}
+
+
 function permAlone(str) {
 
 
