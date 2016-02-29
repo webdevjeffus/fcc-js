39ff75347364241a01ebbe9b81f9b42f3a673620
Jeff George <webdevjeffus@gmail.com>
2016-02-29 18:18:20 -0500

    Add no_repeats.js partial solution that passes 3/7 tests

--- no_repeats.js
+++ no_repeats.js
@@ -1,21 +1,50 @@
 // Solution in progress; not complete
 
-function permute(str) {
-  var chars = str.split("");
-  var perms = [];
+function getPerms(str){
+  var permutations = [],
+    nextPerm = [],
+    chars = str.split('');
 
-  for (var i = 0; i < chars.length; i++ ) {
-
+  function permutate(chars){
+    if( chars.length === 0 ) {
+      permutations.push(nextPerm.join(''));
+    }
+    for( var i = 0; i < chars.length; i++ ){
+      chars.push(chars.shift());
+      nextPerm.push(chars[0]);
+      permutate(chars.slice(1));
+      nextPerm.pop();
+    }
   }
 
+  permutate(chars);
+  console.log(permutations);
+  return permutations;
+}
+
+function checkForRepeats(word) {
+  for( var i = 0; i < word.length - 1; i++) {
+    if ( word[i] === word[i+1]) {
+      return true;
+    }
+    return false;
+  }
 }
 
 
 function permAlone(str) {
+  var allPerms = getPerms(str);
+  console.log(allPerms);
+  var noRepeatsPerms = [];
 
-
-
-  return str;
+  debugger;
+  for( var i = 0; i < allPerms.length; i++ ) {
+    if ( checkForRepeats(allPerms[i]) == false ) {
+      noRepeatsPerms.push(allPerms[i]);
+    }
+  }
+  console.log(noRepeatsPerms);
+  return noRepeatsPerms.length;
 }
 
 permAlone('aab');
